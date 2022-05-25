77a9010b0f5fefba2f9472e1f51197d9a741a4fa
Sohaib-50 <sohaibahmedabbasi0@gmail.com>
2022-05-25 20:09:01 +0500

    deepface recog code snippet updated

--- main.py
+++ main.py
@@ -43,8 +43,9 @@ def main() -> None:
             cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)  # draw rectangle around face
 
             # face = frame[y:y+h, x:x+w]  # extract face from frame
-
-            # recognition = DeepFace.find(face, db_path = "./unidentified_faces")  # recognize face (replace "face" with "np.array(face)" if not working)
+            
+            # face = np.array(face)  # convert to numpy array (for deepface)
+            # recognition = DeepFace.find(face, db_path = "./unidentified_faces")  # recognize face
             # print(recognition)
    
         cv2.imshow("Face Recognition", frame)  # display the current frame
