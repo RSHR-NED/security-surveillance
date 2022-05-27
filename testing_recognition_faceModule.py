8e01f92c576835eb3234183bd78e9a5a51c17017
Rohan Ahmed <rohanahmed212212@gmail.com>
2022-05-28 01:29:09 +0500

    tutorial update

--- testing_recognition_faceModule.py
+++ testing_recognition_faceModule.py
@@ -6,14 +6,15 @@ import face_recognition as fr
 
 def test_recognition_face():
     # Load the jpg file into a numpy array
-    image_M = fr.load_image_file('test_db0/Marshmello0.png')
-    image_M_test = fr.load_image_file('test_db1/Marshmello_test.jpg')
+    test_imgs = ["Marshmello_test.jpg", "Skrillex_test.jpg", "Slushii_test.png", "Valentin_test.jpg"]  # test images paths
+    image_M = fr.load_image_file("test_db1/" + test_imgs[3])  # read test image in array
     image_M = cv2.cvtColor(image_M, cv2.COLOR_BGR2RGB)
-    image_M_test = cv2.cvtColor(image_M_test, cv2.COLOR_BGR2RGB)
-
     face_loc_M = fr.face_locations(image_M)[0]
     face_enc_M = fr.face_encodings(image_M)[0]
 
+
+    image_M_test = fr.load_image_file('test_db0/Valentin0.jpg')
+    image_M_test = cv2.cvtColor(image_M_test, cv2.COLOR_BGR2RGB)
     face_loc_M_test = fr.face_locations(image_M_test)[0]
     face_enc_M_test = fr.face_encodings(image_M_test)[0]
 
