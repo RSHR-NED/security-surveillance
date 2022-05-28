e5d744036daf6da1d4bf859067af6a88a9418330
Rohan Ahmed <rohanahmed212212@gmail.com>
2022-05-28 17:30:44 +0500

    Face Recognition correctly work

--- testing_recognition_faceModule.py
+++ testing_recognition_faceModule.py
@@ -1,4 +1,4 @@
-#import numpy as np
+import numpy as np
 import cv2
 import os
 import face_recognition as fr
@@ -6,7 +6,7 @@ import face_recognition as fr
 
 def test_recognition_face():
     # Load the jpg file into a numpy array
-    image_M = fr.load_image_file("identified_faces/marshmello.png")  # read test image in array
+    image_M = fr.load_image_file("identified_faces/1.png")  # read test image in array
     image_M = cv2.cvtColor(image_M, cv2.COLOR_BGR2RGB)
     face_loc_M = fr.face_locations(image_M)[0]
     face_enc_M = fr.face_encodings(image_M)[0]
@@ -16,7 +16,7 @@ def test_recognition_face():
     input()
 
 
-    image_M_test = fr.load_image_file('test_db0/Valentin0.jpg')
+    image_M_test = fr.load_image_file('unidentified_faces/Marshmello_test.jpg')
     image_M_test = cv2.cvtColor(image_M_test, cv2.COLOR_BGR2RGB)
     face_loc_M_test = fr.face_locations(image_M_test)[0]
     face_enc_M_test = fr.face_encodings(image_M_test)[0]
