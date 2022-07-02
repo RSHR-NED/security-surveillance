15b9cb66169afb78c49e582937f5eeed22a851bd
Sohaib-50 <sohaibahmedabbasi0@gmail.com>
2022-07-02 20:58:41 +0500

    operation flip commit 1

--- helpers.py
+++ helpers.py
@@ -1,6 +1,10 @@
-import face_recognition
 import json
 import os
+from PIL import UnidentifiedImageError
+
+import cv2
+import face_recognition
+import numpy as np
 
 
 class FaceRecognizer:
@@ -11,7 +15,10 @@ class FaceRecognizer:
         print("Initializing FaceRecognizer...\n\n")
         self.identified_faces, self.unidentified_faces = self.load_face_encodings()
         
-
+        self.newest_id = 0 if len(self.identified_faces) == 0 else max(self.unidentified_faces.keys())
+        self.newest_unid = 0 if len(self.unidentified_faces) == 0 else max(self.unidentified_faces.keys())
+        print("FaceRecognizer initialized.\n\n")
+        
 
     def load_face_encodings(self):
         """
@@ -32,8 +39,8 @@ class FaceRecognizer:
                 for filename in os.listdir("./unidentified_faces/"):  # for each image in the folder
                     image_array = face_recognition.load_image_file(("./unidentified_faces/" + filename))  # load image
                     image_encoding = face_recognition.face_encodings(image_array)[0].tolist()  # encode the image, assuming only one face in image
-                    name = filename.split(".")[0]  # get the name of the image (filename without extension)
-                    unidentified_faces[name] = image_encoding  # add the image encoding to the dictionary
+                    id_ = filename.split(".")[0]  # get the name of the image (filename without extension)
+                    unidentified_faces[id_] = image_encoding  # add the image encoding to the dictionary
                 self.save_encodings(unidentified_faces, "./unidentified_faces_encodings.json")  # save the dictionary to json file
                 print("Saved unidentified faces to json file.")
         print("Loaded unidentified faces.\n")
@@ -68,5 +75,59 @@ class FaceRecognizer:
         with open(filename, "w") as f:
             json.dump(content, f)
 
-    
 
+    def recognize_faces(self, frame):
+        """
+        Recognizes faces in the given frame.
+        """
+        # preprocess the frame for face recognition
+        frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)  # resize frame to 1/4th size to reduce processing time
+        frame = frame[:, :, ::-1]  # convert BGR to RGB for face_recognition library's use
+        
+        frame_face_locations = face_recognition.face_locations(frame)  # get face locations
+        frame_face_encodings = face_recognition.face_encodings(frame, )  # get encodings for all faces in frame
+        frame_faces_names = []  # list of names for all faces in frame
+
+        identifed_faces_encodings = []
+        for face_name in self.identified_faces:
+            identifed_faces_encodings.append(self.identified_faces[face_name][0])
+
+        unidentified_faces_encodings = []
+        for face_name in self.unidentified_faces:
+            unidentified_faces_encodings.append(self.unidentified_faces[face_name])
+
+        for face_encoding in frame_face_encodings:
+            identified_faces_matches = face_recognition.compare_faces(identifed_faces_encodings, face_encoding)  # get matches for each face in frame
+            unidentified_faces_matches = face_recognition.compare_faces(unidentified_faces_encodings, face_encoding)  # get matches for each face in frame
+            
+            # check against identified faces
+            identified_face_distances = face_recognition.face_distance(identifed_faces_encodings, face_encoding)  # get distances for each face in frame
+            best_match_index = np.argmin(identified_face_distances)
+            if identified_faces_matches[best_match_index] == True:
+                name = list(self.identified_faces.keys())[best_match_index]
+                is_safe = self.identified_faces[name][1]
+                frame_faces_names.append(f"{name} - ({'safe' if is_safe else 'unsafe'})")
+                continue
+            
+            # check against unidentified faces
+            unidentified_face_distances = face_recognition.face_distance(unidentified_faces_encodings, face_encoding)  # get distances for each face in frame
+            best_match_index = np.argmin(unidentified_face_distances)
+            if unidentified_faces_matches[best_match_index] == True:
+                id_ = list(self.unidentified_faces.keys())[best_match_index]
+                frame_faces_names.append(f"{id_} - (unidentified)")
+                continue
+
+            # face is not recognized, add to unidentified faces
+            id_ = str(uuid.uuid4())
+
+                
+        return frame_faces_names, frame_face_locations
+                
+
+
+
+        # best_match_index = np.argmin(face_distances)
+        # if matches[best_match_index]:
+        #     name = list(self.identified_faces.keys())[best_match_index]
+        #     is_safe = self.identified_faces[name][1]
+            
--- main.py
+++ main.py
@@ -4,38 +4,57 @@ import cv2
 
 def main() -> None:
     face_recognizer = FaceRecognizer()
-    # cam = cv2.VideoCapture(0)
-    # FRAME_RATE = 7
-    # prev = 0
-    # while True:
+    cam = cv2.VideoCapture(0)
+    FRAME_RATE = 30
+    prev = 0
+    while True:
 
-    #     # to exit by pressing "q"
-    #     if cv2.waitKey(1) & 0xFF == ord('q'):
-    #         break
+        # to exit by pressing "q" or esc
+        if cv2.waitKey(1) & 0xFF == ord('q') or cv2.waitKey(1) & 0xFF == 27:
+            break
         
-    #     # limiting to set frame rate
-    #     time_elapsed = time() - prev
-    #     if time_elapsed <= 1/FRAME_RATE:
-    #         continue  # skip this iteration
+        # limiting to set frame rate
+        time_elapsed = time() - prev
+        if time_elapsed <= 1/FRAME_RATE:
+            continue  # skip this iteration
+
+        prev = time()
+
+        ret, frame = cam.read()  # read a frame
+        if not ret:  # if frame is not read correctly
+            print("Error: Failed to read frame from camera")
+            break
+
+        frame = cv2.flip(frame, 1)  # mirror frame horizontally
+
+        # recognize faces in the frame
+        frame_faces_names, frame_face_locations = face_recognizer.recognize_faces(frame)
+
+        # draw rectangles around faces
+        for (top, right, bottom, left), name in zip(frame_face_locations, frame_faces_names):
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+        # display the resulting frame
+        cv2.imshow('Face Recognizer', frame)
+
+
         
-    #     prev = time()
-
-    #     # read a frame
-    #     ret, frame = cam.read()
-    #     if not ret:  # if frame is not read correctly
-    #         print("Error: Failed to read frame from camera")
-    #         break
-    #     frame = cv2.flip(frame, 1)  # mirror frame horizontally
-
-    #     images = ImageDatabase.read_identified_faces_db("./identified_faces")
-    #     E.find_encodings(images)
-    #     encodings = E.get_all_encodings()
-
-    #     FR.comparing_faces(frame,encodings)
-    #     cv2.imshow("Face Recognition", frame)  # display the current frame
-        
-    # cam.release()
-    # cv2.destroyAllWindows()
+    cam.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
